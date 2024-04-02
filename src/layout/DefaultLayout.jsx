94cfaaf6cc2da813d8dbd5be654ec7d7ab66aa31
Ernest Haruna <ernestharuna1@gmail.com>
2024-04-02 22:49:42 +0100

    Created side nav

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -2,9 +2,40 @@ import { Outlet } from "react-router-dom";
 
 export default function DefaultLayout() {
     return (
-        <div>
-            DefaultLayout
-            <Outlet />
+        <div className="px-10 font-sans">
+            <header className="my-5">
+                <h1 className="font-semibold text-3xl">Chart of accounts</h1>
+                <p>Configure and approve your chart of account for your business sector</p>
+            </header>
+
+            <section className="grid grid-cols-6 gap-5">
+                <aside>
+                    <h2 className="font-bold mb-5">Accounts</h2>
+                    <ul className="border-l border-neutral-300 font-semibold">
+                        <li className="py-1 px-3 border-s-4 border-green-400 text-green-400">
+                            Economic
+                        </li>
+                        <li className="py-1 px-3">
+                            Administrative
+                        </li>
+                        <li className="py-1 px-3">
+                            Geocode
+                        </li>
+                        <li className="py-1 px-3">
+                            Function
+                        </li>
+                        <li className="py-1 px-3">
+                            Food
+                        </li>
+                        <li className="py-1 px-3">
+                            Programme
+                        </li>
+                    </ul>
+                </aside>
+                <main className="col-span-5">
+                    <Outlet />
+                </main>
+            </section>
         </div>
     )
 }
--- src/views/Economic.jsx
+++ src/views/Economic.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Economic() {
     return (
         <h1 className="text-3xl font-bold underline">
-            Hello world!
+            Economic
         </h1>
     )
 }
